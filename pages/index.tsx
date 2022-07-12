7b9673f9f8c836b91af763fe5048032aecca8b7e
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-07-12 19:23:41 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router'
+import Confetti from 'react-confetti'
 import React, { useState, useEffect } from "react"
 
 export async function getServerSideProps({ query }) {
@@ -39,6 +40,11 @@ export default function App({ queries }) {
 	
 	return (
 		<main className="flex dark:bg-gray-800 w-screen h-screen">
+                        {count == objetive ?
+                          <Confetti/>
+                           :
+                          null
+                        }
 			<input className="outline-none font-bold text-gray-500 dark:text-gray-300 bg-transparent top-1/4 left-24 text-center absolute" type="text" placeholder="Counter Name" onChange={(text) => {
 			  setText(text.target.value)
 			  save(currentText, count, objetive)
