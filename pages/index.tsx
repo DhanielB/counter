566aa8a27fd15307f012803b0d3ff27e96a5f7d8
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-07-12 19:30:16 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,7 +43,7 @@ export default function App({ queries }) {
                         {count === objetive ?
                           <Confetti/>
                             :
-                          null
+                          null}
 			<input className="outline-none font-bold text-gray-500 dark:text-gray-300 bg-transparent top-1/4 left-24 text-center absolute" type="text" placeholder="Counter Name" onChange={(text) => {
 			  setText(text.target.value)
 			  save(currentText, count, objetive)
