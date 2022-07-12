6dd37fa2deffdb5eb1ce3cf413547822f9897238
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-07-12 19:26:04 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,11 +40,7 @@ export default function App({ queries }) {
 	
 	return (
 		<main className="flex dark:bg-gray-800 w-screen h-screen">
-                        {count == objetive ?
-                          <Confetti/>
-                           :
-                          null
-                        }
+                        <Confetti numberOfPieces={count == objetive ? 200  : 0}/>
 			<input className="outline-none font-bold text-gray-500 dark:text-gray-300 bg-transparent top-1/4 left-24 text-center absolute" type="text" placeholder="Counter Name" onChange={(text) => {
 			  setText(text.target.value)
 			  save(currentText, count, objetive)
